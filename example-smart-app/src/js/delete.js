8948ca349bc4935bef592e23c71b0c68c114b52d
Steven <sluong30@gatech.edu>
2023-11-30 19:20:28 -0800

    minor update

--- example-smart-app/src/js/delete.js
+++ example-smart-app/src/js/delete.js
@@ -1,17 +1,31 @@
-$(document).on('click', '.btn.btn-outline-danger', function() {
-    var medicationOrderId = this.id; // The id of the button is the MedicationOrder resource id
-  
-    smartClient.patient.api.delete({
-      type: 'MedicationOrder',
+$(document).on("click", ".btn.btn-outline-danger", function () {
+  var medicationOrderId = this.id; // The id of the button is the MedicationOrder resource id
+
+  // Show the loading overlay
+  $("#loadingOverlay").show();
+
+  smartClient.patient.api
+    .delete({
+      type: "MedicationOrder",
       id: medicationOrderId,
-    }).then(function(response) {
-      // The medication order was successfully deleted
-      console.log('Medication order deleted:', response);
-  
-      // Optionally, remove the row from the table
-      $('#' + medicationOrderId).closest('tr').remove();
-    },function(error) {
-      // An error occurred
-      console.error('Error deleting medication order:', error);
-    });
-  });
+    })
+    .then(
+      function (response) {
+        // The medication order was successfully deleted
+        console.log("Medication order deleted:", response);
+
+        // Remove the row from the table
+        $("#" + medicationOrderId)
+          .closest("tr")
+          .remove();
+
+        // Hide the loading overlay
+        $("#loadingOverlay").hide();
+      },
+      function (error) {
+        // An error occurred
+        console.error("Error deleting medication order:", error);
+        $("#loadingOverlay").hide();
+      }
+    );
+});
